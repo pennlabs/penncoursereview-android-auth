ee8cec5b1db52a274315d63179315b9413941eb6
CIS-350 PCR Android App App <labs@pennapps.com>
2012-10-13 20:58:36 -0400

    Some aesthetic changes

--- getserial/index.php
+++ getserial/index.php
@@ -16,14 +16,17 @@
 	$readable_err_str = API_Strings::HUMAN_READABLE_ERROR;
 ?>
 <html>
-<body>
+<body bgcolor=#E3E3E3>
+<font color=#333333>
 	Welcome, <b><? echo($_SERVER['REMOTE_USER']) ?></b>!<br />
 	<p>Here, you can obtain a serial number to activate <b>Penn Course Review Mobile</b> on your device.</p>
-
+</font>
 <?
 	function display_serial($_serial){
 		echo('
-			<p><h2><b><font color="#00AA00">Your serial number is: </font></b>' . $_serial . '</h2></p>	
+			<div style="text-align:center;"><h2><b><font color="#154381">Your serial number is: </font></b></br>' . $_serial . '</h2></div>
+			<div style="text-align:center;">Copy this serial number into the login prompt</div>
+	
 		');
 	}
 
@@ -60,8 +63,8 @@
 		mysql_close($conn);
 	}
 ?>
-
+<font color="#333333" size=2>
 	<p><b>Why do I need a serial number?</b>  For security and privacy, Penn Course Review is only accessible to users with a valid PennKey.  Penn Course Review Mobile is a standalone mobile application, and Penn's Information Systems & Computing (ISC) department currently does not support handling secure PennKey logins on mobile devices.</p><p>So, this is our ISC-approved solution to get around this: You already logged in to this page using Penn's official WebLogin system, and this page will give you a serial number that will activate your mobile app.  And don't worry - You only need to enter the serial number once.  Once your app is activated, you will be able to use Penn Course Review Mobile instantly, for as long as your PennKey is valid.  Just make sure that you <b>never give your serial number out to anybody</b>.</p>
-
+</font>
 </body>
 </html>
